bec7e869d52d38f2ca020e60505876b27dafba2e
DangThaiHoa <dangthaihoalol@gmail.com>
2023-03-27 15:37:06 +0700

    Complete WeatherInformation

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -3,6 +3,8 @@
 #include <ESP8266WiFi.h>
 #include "FirebaseESP8266.h"
 #include <DHT.h>
+#include <ThreeWire.h>  
+#include <RtcDS1302.h>
 
 //ESP8266/FireBase config
 #define WIFI_SSID "Home 2.4Ghz"
@@ -21,10 +23,28 @@ FirebaseJson json;
 #define DHTTYPE DHT11  
 DHT dht(DHTPIN, DHTTYPE);
 
+//WaterSensor config
+#define WATERSENSORPIN A0    
+
+//DS1302 config
+// CONNECTIONS:
+// DS1302 CLK/SCLK --> D2
+// DS1302 DAT/IO --> D3
+// DS1302 RST/CE --> D4
+// DS1302 VCC --> 3.3v - 5v
+// DS1302 GND --> GND
+ThreeWire myWire(D3,D2,D4); // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
 //Var
 float Hum; 
 float Temp;
 
+int WaterData;
+
+char datestring[20];
+char timestring[20];  
 
 void setup() {
   Serial.begin(115200);
@@ -47,17 +67,54 @@ void setup() {
 
   //DHT config
   dht.begin();
+
+  //DS1302 config
+  Rtc.Begin();
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  if (!Rtc.IsDateTimeValid()) 
+    {
+        Serial.println("RTC lost confidence in the DateTime!");
+        Rtc.SetDateTime(compiled);
+    }
+
+    if (Rtc.GetIsWriteProtected())
+    {
+        Serial.println("RTC was write protected, enabling writing now");
+        Rtc.SetIsWriteProtected(false);
+    }
+
+    if (!Rtc.GetIsRunning())
+    {
+        Serial.println("RTC was not actively running, starting now");
+        Rtc.SetIsRunning(true);
+    }
+
+    RtcDateTime now = Rtc.GetDateTime();
+    if (now < compiled) 
+    {
+        Serial.println("RTC is older than compile time!  (Updating DateTime)");
+        Rtc.SetDateTime(compiled);
+    }
 }
 
 void loop() {
   delay(500);
+
   //Get Hum & Temp
+  getDHT11();
+
+  //Get Water Data
+  getWaterSensor();
+
+  //Get DateTime
+  getDS1302();
+
+}
+
+void getDHT11(){
   Hum = dht.readHumidity();
   Temp = dht.readTemperature();
-  Serial.println(Temp);
-  Serial.println(Hum);  
 
-  //Update Hum & Temp Data FireBase
   if (isnan(Hum) || isnan(Temp)){
     Serial.println("Error read DHT11");
     Firebase.setString(FBData, path + "/DHT11/error", "Read/Connect");
@@ -67,3 +124,39 @@ void loop() {
     Firebase.setFloat(FBData, path + "/DHT11/temp", Temp);
   }
 }
+
+void getWaterSensor(){
+  WaterData = analogRead(WATERSENSORPIN);
+  Firebase.setInt(FBData, path + "/WATERSENSOR/waterdata", WaterData);
+}
+
+void getDS1302(){
+  RtcDateTime now = Rtc.GetDateTime();
+    if (!now.IsValid())
+    {
+      Firebase.setString(FBData, path + "/DS1302/error", "Read/Connect/Battery"); 
+    }else{
+      printDateTime(now);
+      Firebase.setString(FBData, path + "/DS1302/error", "Non");    
+      Firebase.setString(FBData, path + "/DS1302/date", datestring);
+      Firebase.setString(FBData, path + "/DS1302/time", timestring);   
+    }
+}
+
+void printDateTime(const RtcDateTime& dt)
+{
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u"),
+            dt.Month(),
+            dt.Day(),
+            dt.Year());
+    snprintf_P(timestring, 
+            countof(timestring),
+            PSTR("%02u:%02u:%02u"),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+}
+
+
